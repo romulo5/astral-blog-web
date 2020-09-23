c15a904ec7b2447aab6fd75d97bfaa3c3e8b6a80
romulo5 <romulofloresta@gmail.com>
2020-09-23 20:39:54 -0300

    added sanity plugin and tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'n5jz1yup',
+        dataset: 'production',
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.MY_SANITY_TOKEN,
+      },
+    },
+    `gatsby-plugin-postcss`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
