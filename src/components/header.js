f979b9499506d3306dd2b4a3a5bf6ea6d8b4042c
romulo5 <romulofloresta@gmail.com>
2020-10-15 20:49:39 -0300

    refactor header design

--- src/components/header.js
+++ src/components/header.js
@@ -33,6 +33,12 @@ const Header = () => {
           alt
           ...SanityImage
         }
+        author {
+          image {
+            alt
+            ...SanityImage
+          }
+        }
       }
     }
   `)
@@ -41,7 +47,6 @@ const Header = () => {
 
   const [isSticky, setSticky] = useState(false)
   const [height, setHeight] = useState({})
-  const [offset, setOffset] = useState(0)
   // const changeHeader = () => {
   //   if (window.scrollY > 200) {
   //     setStickyHeader(true)
@@ -71,66 +76,95 @@ const Header = () => {
   }
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll)
     const navbar = document.getElementById("navbar")
     const header = document.getElementById("header")
+    window.addEventListener("scroll", handleScroll)
     return () => {
       window.removeEventListener("scroll")
     }
   }, [])
 
   return (
-    <>
-      <header className={`${isSticky ? " " : ""} h-auto flex w-full `}>
-        <div
-          className={`  ${
-            isSticky ? "fixed bg-white z-50 h-16  " : ""
-          } transform ease-linear flex flex-col justify-end items-center pb-6  shadow-2xl  mb-6 w-full`}
-          id="header"
-        >
-          {data.sanitySiteSettings.image &&
-            data.sanitySiteSettings.image.asset && (
-              <img
-                src={imageUrlFor(buildImageObj(data.sanitySiteSettings.image))
-                  .height(300)
-                  .auto("format")
-                  .url()}
-                alt={data.sanitySiteSettings.image.alt}
-                className={` ${
-                  isSticky ? "" : " "
-                } w-full object-contain xl:object-cover xl:h-64 object-center`}
-              />
-            )}
-          <h1 className="flex text-4xl sm:text-5xl py-5">
-            <Link
-              to="/"
+    <header>
+      <div
+        className={`${
+          isSticky ? "fixed bg-white z-50 h-16 " : "pb-6 flex-col"
+        } transform ease-linear flex  items-center  shadow-2xl  mb-6 w-full`}
+        id="header"
+      >
+        {data.sanitySiteSettings.image &&
+          data.sanitySiteSettings.image.asset && (
+            <img
+              src={imageUrlFor(buildImageObj(data.sanitySiteSettings.image))
+                .height(300)
+                .auto("format")
+                .url()}
+              alt={data.sanitySiteSettings.image.alt}
               className={` ${
-                isSticky ? "hidden " : " "
-              } text-gray-700 hover:shadow-none`}
-            >
+                isSticky ? "hidden" : "flex"
+              } w-full object-contain xl:object-cover xl:h-64 object-center `}
+            />
+          )}
+        <Link
+          to="/"
+          className={`${isSticky ? "hover:shadow-none px-5" : "hidden"}`}
+        >
+          <img
+            src={
+              data.sanitySiteSettings.author.image &&
+              imageUrlFor(buildImageObj(data.sanitySiteSettings.author.image))
+                .width(40)
+                .height(40)
+                .url()
+            }
+            alt={data.sanitySiteSettings.author.image.alt}
+            className="transition duration-300 ease-in-out sm:scale-125 transform hover:scale-150 rounded-full m-0"
+          />
+        </Link>
+        <div
+          className={`flex w-full ${
+            isSticky
+              ? "text-base pr-5  "
+              : "  md:flex-row flex-col justify-evenly items-center"
+          }  `}
+        >
+          {" "}
+          <h1
+            className={`flex ${
+              isSticky
+                ? "text-base pr-5 hidden sm:flex"
+                : "  text-3xl sm:text-4xl"
+            }  py-5 text-gray-700 `}
+          >
+            <Link to="/" className="hover:shadow-none">
               {data.sanitySiteSettings.title}
             </Link>
           </h1>
-          <nav id="navbar" className={` ${isSticky ? " " : " "} flex `}>
-            <Link to="/" className="sm:text-xl  text-gray-700 px-2">
+          <nav
+            id="navbar"
+            className={`flex  justify-self-center  text-gray-700  ${
+              isSticky ? "self-center text-sm sm:text-base" : "sm:text-xl "
+            } `}
+          >
+            <Link to="/" className="px-2">
               comTexto
             </Link>
             <span> · </span>
-            <Link to="/" className="sm:text-xl text-gray-700 px-2">
+            <Link to="/" className="px-2">
               comVerso
             </Link>
             <span> · </span>
-            <Link to=" " className="sm:text-xl text-gray-700 px-2">
+            <Link to=" " className="px-2">
               comFoto
             </Link>
           </nav>
         </div>
-        <div
-          className={` ${isSticky ? "flex" : "hidden "} `}
-          style={{ height: height.heightValue }}
-        />
-      </header>
-    </>
+      </div>
+      <div
+        className={` ${isSticky ? "flex" : "hidden "} `}
+        style={{ height: height.heightValue }}
+      />
+    </header>
   )
 }
 
